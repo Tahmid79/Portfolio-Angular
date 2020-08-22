33866cda01e610479c00f8ed216ef432a793a36f
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-08-22 20:55:00 +0600

    Tech Stack

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,11 +8,58 @@ import { Component, OnInit } from '@angular/core';
 export class SkillsComponent implements OnInit {
 
   numbers  ;
-  constructor() {
-    this.numbers = Array(12).fill(0).map((x,i)=>i);
+
+
+  skills = [] ;
+
+  str ;
+
+  back = "url(assets/stack/rct.jpg)" ;
+
+
+
+  init(){
+    this.skills = [
+      { b1 :  this.str +"rct.jpg)"} ,
+      { b1 :  this.str+"ang.jpg)"} ,
+      { b1 :  this.str+"redux.jpg)"} ,
+      { b1 :  this.str+"node.jpg)"} ,
+
+      { b1 :  this.str+"java.png)"},
+      { b1 :  this.str+"c.png)"},
+      { b1 :  this.str+"python.jpg)"},
+
+      { b1 :  this.str+"django.jpg)"},
+      { b1 :  this.str+"js.jpg)"} ,
+      { b1 :  this.str+"css.jpg)"},
+
+      { b1 :  this.str+"html.jpg)"},
+      { b1 :  this.str+"android.png)"},
+      { b1 :  this.str + "numpy.png)"},
+      { b1 :  this.str + "keras.jpg)"},
+      { b1 :  this.str + "sklearn.jpg)"},
+
+    ] ;
   }
 
+
+
+  constructor() {
+    this.numbers = Array(2).fill(0).map((x,i)=>i);
+    this.str = "url(assets/stack/" ;
+    this.init() ;
+
+  }
+
+
+
+  getBack(index){
+      return this.skills[index].b1;
+  }
+
+
   ngOnInit(): void {
+
   }
 
 }
